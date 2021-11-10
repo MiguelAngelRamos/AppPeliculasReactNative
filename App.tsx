e22329572a5336a4204163ca14103d0d5403d1a6
Miguel Ramos <return.infox@gmail.com>
2021-11-10 05:59:14 -0300

    APP DE PELICULAS REACT NATIVE NAVIGATION INSTALADO

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Componente principal</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
